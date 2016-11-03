7927d6d9e283f7deadb424569c7bbcfaff070416
vsundareshan <vsundareshan@CHIC02QV06ZG8WN.sea.corp.expecn.com>
2016-11-03 00:23:17 -0500

    Initial code

---
+++ app/src/main/java/hw3/sndrshn2/cs465/cs465_hw3/MainActivity.java
@@ -0,0 +1,14 @@
+package hw3.sndrshn2.cs465.cs465_hw3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        setContentView(R.layout.activity_main);
+    }
+}
